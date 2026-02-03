ce60d6f18e73a0fef0cd3b4273d4592a4376c8ed
Aleksey Udovenko <aleksey.udovenko@gmail.com>
2026-02-03 14:19:07 +0300

    Create TradingViewWidget.tsx

---
+++ components/TradingViewWidget.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react';
+
+const TradingViewWidget = () => {
+  const container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (container.current && !container.current.querySelector('script')) {
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = JSON.stringify({
+        "autosize": true,
+        "symbol": "MYX:FCPO1!",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "1",
+        "locale": "en",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "calendar": false,
+        "support_host": "https://www.tradingview.com"
+      });
+      container.current.appendChild(script);
+    }
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
+      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
+    </div>
+  );
+};
+
+export default TradingViewWidget;
