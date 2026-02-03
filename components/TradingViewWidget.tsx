a4df4e48609ed42320c2932282d0785a497a239d
Aleksey Udovenko <aleksey.udovenko@gmail.com>
2026-02-03 14:25:44 +0300

    Fix JSON syntax in TradingViewWidget script

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -11,7 +11,7 @@ const TradingViewWidget = () => {
       script.async = true;
       script.innerHTML = JSON.stringify({
         "autosize": true,
-        "symbol": "TVC:CPO"
+        "symbol": "TVC:CPO",
         "interval": "D",
         "timezone": "Etc/UTC",
         "theme": "light",
